b2f07edc83332e764fb33f29749e33f51d3b4cf2
mlebl <mlebl@github.com>
2017-02-16 12:20:19 -0500

    Started Travis CI integration
    Signed-off-by: Arevor <arevor@github.com>

--- src/main/java/addressbook/MyController.java
+++ src/main/java/addressbook/MyController.java
@@ -3,10 +3,13 @@ package addressbook;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/")
 public class MyController {
 
     private final GreetingService service;
@@ -15,6 +18,13 @@ public class MyController {
         this.service = service;
     }
 
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView index() {
+        ModelAndView modelAndView = new ModelAndView("app/index");
+        return modelAndView;
+    }
+
+    /*
     @RequestMapping("/")
     public @ResponseBody String home() {
         return "Hello World!!";
@@ -25,7 +35,7 @@ public class MyController {
     public @ResponseBody String greeting() {
         return service.greet();
     }
-
+    */
 
 }
 
